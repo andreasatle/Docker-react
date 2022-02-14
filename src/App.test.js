f10c79a50fd41f11e0eea2505f0885bee4f4a5be
Andreas Atle <atle.andreas@gmail.com>
2022-02-14 09:16:51 -0600

    Make two tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,13 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/foo bar/i);//(/learn react/i);
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
 
+
+test('failing test', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/foo bar/i);
+  expect(linkElement).toBeInTheDocument();
+});
